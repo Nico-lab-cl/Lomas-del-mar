09761df15424b62a3a6c0f0d8eb16ea77c24b80d
Nico Lab Dev <dev@nicolab.cl>
2026-02-11 19:15:44 -0300

    fix: Add NextAuth type definition for User role

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,28 @@
+
+import { Role } from "@prisma/client"
+import NextAuth, { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            /** The user's role. */
+            role: Role
+            id: string
+        } & DefaultSession["user"]
+    }
+
+    interface User {
+        role: Role
+    }
+}
+
+declare module "next-auth/jwt" {
+    /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+    interface JWT {
+        role: Role
+        id: string
+    }
+}
